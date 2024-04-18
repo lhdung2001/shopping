3ac6bbbc5fc257cc2f949615d7e5ef2e0deace6f
lhdung2001 <lhdung2001@gmail.com>
2024-04-18 13:49:03 +0700

    init project

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2024 Shopping Portal by LHD </b> All rights reserved.
+		</div>
+	</div>
---
+++ includes/footer.php
@@ -0,0 +1,120 @@
+<footer id="footer" class="footer color-bg">
+    <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT INFO ============================================================= -->
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+
+                                    <h3>Shopping Portal</h3>
+                                </a>
+                            </div><!-- /.logo -->
+
+                        </div><!-- /.footer-logo -->
+
+                        <div class="module-body m-t-20">
+                            <p class="about-us"> Hello, Cảm ơn bạn tới đến với Shopping Portal <br>
+                                mua sắm thoải mái không lo về giá.</p>
+
+                            <div class="social-icons">
+
+                                <a href="https://www.facebook.com/lhdung2001" class='active'><i class="icon fa fa-facebook"></i></a>
+                                <a href="#"><i class="icon fa fa-twitter"></i></a>
+                                <a href="#"><i class="icon fa fa-linkedin"></i></a>
+                                <a href="#"><i class="icon fa fa-rss"></i></a>
+                                <a href="#"><i class="icon fa fa-pinterest"></i></a>
+
+                            </div><!-- /.social-icons -->
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-info -->
+                    <!-- ============================================================= CONTACT INFO : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT TIMING============================================================= -->
+                    <div class="contact-timing">
+                        <div class="module-heading">
+                            <h4 class="module-title">opening time</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td>Monday-Friday:</td>
+                                            <td class="pull-right">8h-18h</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Saturday:</td>
+                                            <td class="pull-right">9h-20h</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sunday:</td>
+                                            <td class="pull-right">10h-20h</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div><!-- /.table-responsive -->
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= CONTACT TIMING : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= INFORMATION============================================================= -->
+                    <div class="contact-information">
+                        <div class="module-heading">
+                            <h4 class="module-title">information</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <ul class="toggle-footer" style="">
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>Thủ Đức,HCM</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>035488130<br></p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span>lhdung2001@gmail.com</span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= INFORMATION : END ============================================================= -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
